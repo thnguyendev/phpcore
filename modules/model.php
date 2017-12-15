48302e33a5c3b2c18e966d706e09abee2bc179bc
thnguyendev <34552270+thnguyendev@users.noreply.github.com>
2017-12-15 11:29:13 -0600

    Update model.php

--- modules/model.php
+++ modules/model.php
@@ -5,7 +5,7 @@
 	// define folder for all of models, the name ModelFolder should not be changed
 	define("ModelFolder", "models/", true);
 
-	// decalre models here
+	// declare models here
 	$ModelList = array (
 		// eg.:
 		// "mymodel" => array ("file" => "mymodel.php")
